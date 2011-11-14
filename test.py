39deca91709b62fe82dfb4126d3bae04accf5c70
Breadbros Integration McRobit <ben.mcgraw+breadbot.github@gmail.com>
2011-11-14 08:04:03 +0000

    Can we fail only on even builds?

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import time
 import unittest
 
 class Test(unittest.TestCase):
-    def test_fail(self):
-        print "BUILD_NUMBER(%s)" % (os.environ['BUILD_NUMBER'])
-	self.fail()
-        pass
+    def test_fail_on_even_builds(self):
+	if int(os.environ['BUILD_NUMBER']) % 2 == 0:
+	    self.fail()
+        else:
+            pass
